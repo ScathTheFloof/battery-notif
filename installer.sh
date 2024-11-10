07b55df5653507f6b1aeb39a7f71aa633227a114
ScathTheFloof <skylineprotofen@gmail.com>
2024-11-09 21:49:42 -0600

    fixed installer.sh not enabling service

--- installer.sh
+++ installer.sh
@@ -9,6 +9,8 @@ sudo cp battery-notif.sh /usr/local/bin/
 cp service/battery-notif.service ~/.config/systemd/user/
 cp service/battery-notif.timer ~/.config/systemd/user/
 
+systemctl --user enable battery-notif.service
+
 echo "
         _____________________________
        /			    /
